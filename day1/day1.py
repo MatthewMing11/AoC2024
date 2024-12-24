579b5b10808d4a267f5290061a36f5930f5eb30f
MatthewMing11 <matthew.ming11@myhunter.cuny.edu>
2024-12-24 03:08:10 -0500

    added day1 python solution

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,15 @@
 # Python script for day 1
+file = open("input.txt","r")
+total = 0
+alist=[]
+blist=[]
+for line in file:
+    a,b=line.split()
+    alist.append(a)
+    blist.append(b)
+alist=sorted(alist)
+blist=sorted(blist)
+for i in range(1000):
+    total+=abs(int(alist[i])-int(blist[i]))
+print(total)
+
