ad69c2d9dff46eb08c3f82c3c23c36ffdd3522c7
MatthewMing11 <matthew.ming11@myhunter.cuny.edu>
2024-12-30 21:48:17 -0500

    added day5 and day6 python solutions

--- day5/day5.py
+++ day5/day5.py
@@ -1,1 +1,68 @@
 # Python script for day 5
+file = open('input.txt','r')
+rules =True
+ordering=dict()
+correct=[]
+for line in file:
+    if line=="\n":
+        rules=False
+        continue
+    if rules:
+        first,second=line.split("|")
+        second= second[:len(second)-1]
+        if first not in ordering:
+            ordering[first]=[];
+        ordering[first].append(second)
+    else:
+        updates=line.split(",")
+        updates[-1]=updates[-1][:len(updates[-1])-1]
+        right=True
+        for i in range(len(updates)-1):
+            for j in range(i+1, len(updates)):
+                if updates[j] not in ordering[updates[i]]:
+                    right=False
+                    break
+            if not right:
+                break
+        if right:
+            correct.append(updates)
+total=0
+for coupdates in correct:
+    total+=int(coupdates[int(len(coupdates)/2)])
+print(total)
+#part 2
+file = open('input.txt','r')
+rules =True
+ordering=dict()
+correct=[]
+for line in file:
+    if line=="\n":
+        rules=False
+        continue
+    if rules:
+        first,second=line.split("|")
+        second= second[:len(second)-1]
+        if first not in ordering:
+            ordering[first]=[];
+        ordering[first].append(second)
+    else:
+        updates=line.split(",")
+        updates[-1]=updates[-1][:len(updates[-1])-1]
+        right=True
+        for i in range(len(updates)-1):
+            for j in range(i+1, len(updates)):
+                if updates[j] not in ordering[updates[i]]:
+                    right=False
+                    break
+            if not right:
+                break
+        if not right:
+            common=[]
+            for i in range(len(updates)):
+                common.append(len([x for x in updates if x in ordering[updates[i]]]))
+            new_updates=[val for _,val in reversed(sorted(zip(common,updates)))]
+            correct.append(new_updates)
+total=0
+for coupdates in correct:
+    total+=int(coupdates[int(len(coupdates)/2)])
+print(total)
--- day6/day6.py
+++ day6/day6.py
@@ -1,1 +1,87 @@
 # Python script for day 6
+file = open('input.txt','r')
+directions=["^",">","v","<",]
+updates=[(-1,0),(0,1),(1,0),(0,-1)]
+map=[]
+obstacles=[]
+position=()
+direction=0
+for line in file:
+    row=[]
+    for c in line:
+        if c!="\n":
+            row.append(c)
+    map.append(row)
+for row in range(len(map)):
+    for col in range(len(map[0])):
+        if map[row][col] == "#":
+            obstacles.append((row,col))
+        if map[row][col] in directions:
+            position=(row,col)
+            direction=directions.index(map[row][col])
+while position[0]>-1 and position[1]>-1 and position[0]<len(map) and position[1]<len(map[0]):
+    if map[position[0]][position[1]]!="X":
+        map[position[0]][position[1]]="X"
+    newpos = tuple(sum(x) for x in zip(position,updates[direction]))
+    if newpos[0]<0 or newpos[1]<0 or newpos[0]>=len(map) or newpos[1]>=len(map[0]):
+        break
+    if map[newpos[0]][newpos[1]]=="#":
+        direction=(direction+1)%4
+        continue
+    position=newpos
+    map[newpos[0]][newpos[1]]=directions[direction]
+total=0
+for row in range(len(map)):
+    for col in range(len(map[0])):
+        if map[row][col]=="X":
+            total+=1
+print(total)
+#part 2
+file = open('input.txt','r')
+directions=["^",">","v","<",]
+updates=[(-1,0),(0,1),(1,0),(0,-1)]
+map=[]
+obstacles=[]
+position=()
+direction=0
+for line in file:
+    row=[]
+    for c in line:
+        if c!="\n":
+            row.append(c)
+    map.append(row)
+for row in range(len(map)):
+    for col in range(len(map[0])):
+        if map[row][col] == "#":
+            obstacles.append((row,col))
+        if map[row][col] in directions:
+            position=(row,col)
+            direction=directions.index(map[row][col])
+originalmap=[row[:] for row in map]
+originalpos = position
+originaldir = direction
+total=0
+for row in range(len(map)):
+    for col in range(len(map[0])):
+        if originalmap[row][col]!="#":
+            map=[r[:] for r in originalmap]
+            map[row][col]="#"
+            position = originalpos
+            direction = originaldir
+            passedob=[]
+            while position[0]>-1 and position[1]>-1 and position[0]<len(map) and position[1]<len(map[0]):
+                if map[position[0]][position[1]]!="X":
+                    map[position[0]][position[1]]="X"
+                newpos = tuple(sum(x) for x in zip(position,updates[direction]))
+                if newpos[0]<0 or newpos[1]<0 or newpos[0]>=len(map) or newpos[1]>=len(map[0]):
+                    break
+                if map[newpos[0]][newpos[1]]=="#":
+                    if (newpos,direction) in passedob:
+                        total+=1
+                        break
+                    passedob.append((newpos,direction))
+                    direction=(direction+1)%4
+                    continue
+                position=newpos
+                map[newpos[0]][newpos[1]]=directions[direction]
+print(total)
